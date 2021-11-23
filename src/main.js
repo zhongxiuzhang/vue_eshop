843aeabed34c8c527b46527a434bff3f56606d4b
Zhongxiu ZHANG <zhongxiuzhang@163.com>
2021-11-23 16:10:22 +0800

    complete the development of good functionality

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import './assets/fonts/iconfont.css'
 // import global stylesheet
 import './assets/css/global.css'
 import TreeTable from 'vue-table-with-tree-grid'
+// import vue-quill-editor
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 
 import axios from 'axios'
 // set the root path of request
@@ -22,6 +27,19 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+// 将富文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 +'').padStart(2,'0')
+  const d = (dt.getDate() + '').padStart(2,'0')
+  const hh = (dt.getHours() + '').padStart(2,'0')
+  const mm = (dt.getMinutes() + '').padStart(2,'0')
+  const ss = (dt.getSeconds() + '').padStart(2,'0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
