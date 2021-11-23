47f46212cdf3a5d97303087c90e07e312ff77349
Zhongxiu ZHANG <zhongxiuzhang@163.com>
2021-11-23 21:30:45 +0800

    complete the development of order functionality

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, 
 Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, 
 Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane, Steps, Step, 
-CheckboxGroup, Checkbox, Upload } from 'element-ui'
+CheckboxGroup, Checkbox, Upload, Timeline, TimelineItem} from 'element-ui'
+
+// import Timeline from './timeline/index.js'
+// import TimelineItem from './timeline-item/index.js'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -39,6 +42,8 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
